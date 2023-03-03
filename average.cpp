874b1c04d7f880a7bbfe928704bbf4209c03bc59
Mind <nathapat.mind@gmaill.com>
2023-03-03 15:20:23 +0700

    final

--- average.cpp
+++ average.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+int main(int argc, char *argv[]){
+
+    if(argc > 1){
+        cout << "---------------------------------\n";
+        float sum;
+        for(int i=1 ; i < argc; i++){
+        sum += atof(argv[i]);
+        }
+        sum = sum/(argc-1);
+        cout << "Average of " << argc-1  << " numbers = " << sum << "\n";
+        cout << "---------------------------------\n";
+    }else{
+        cout << "Please input numbers to find average.\n";
+    }
+}
